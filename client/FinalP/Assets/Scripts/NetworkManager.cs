80d25d49538901c67279c8ab9e991861b0135419
monster356 <ruksasut@hotmail.com>
2019-05-22 19:29:37 +0700

    network Script

---
+++ client/FinalP/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkManager : MonoBehaviour
+{
+    static SocketIOComponent socket;
+    public GameObject playerPrefab;
+    float x,z;
+
+    void Start()
+    {
+
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+
+        socket.On("spawn", OnSpawn);
+
+        socket.On("move", OnMove);
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnConnected(SocketIOEvent e)
+    {
+        print("connected");
+
+        socket.Emit("move");
+
+    }
+    void OnSpawn(SocketIOEvent e)
+    {
+        Vector3 position = new Vector3(0f, 1f, 0f);
+        print("spawned");
+
+        Instantiate(playerPrefab);
+        //socket.Emit("move");
+
+    }
+    void OnMove(SocketIOEvent e)
+    {
+        print("player is moving " + e.data);
+    }
+}
